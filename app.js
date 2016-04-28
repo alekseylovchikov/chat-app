ce7de7bf5ae3aa14de8c87d734712775aa12b8b9
alekseylovchikov <svdmusic@gmail.com>
2016-04-28 13:44:24 +0300

    fix npm start

--- app.js
+++ app.js
@@ -1,27 +1,37 @@
 var express = require('express');
 var app = express();
+var port = process.env.PORT || 3000;
+// load json data
 var rooms = require('./data/rooms.json');
 
+// set view engine jade
 app.set('view engine', 'jade');
 app.set('views', './views');
 
+// include static files
 app.use(express.static('public'));
 app.use(express.static('node_modules/bootstrap/dist'));
 
+// home page
 app.get('/', function(req ,res) {
     res.render('index', {
         title: 'Home'
     });
 });
 
+// view all rooms
 app.get('/admin/rooms', function(req, res) {
-    
     res.render('rooms', {
         title: 'Chat Rooms',
         rooms: rooms
     });
 });
 
-app.listen(3000, function() {
-	console.log('open: http://localhost:3000');
+app.get('/admin/rooms/add', function(req, res) {
+    res.render('add');
+});
+
+// listen server
+app.listen(port, function() {
+	console.log('open: http://localhost:' + port);
 });
